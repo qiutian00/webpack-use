ee48164b00899b4c8c050198b65d8f67d0c6c342
liwen <1476102147@qq.com>
2019-11-04 22:42:33 +0800

    feat: update to show

--- src/main.js
+++ src/main.js
@@ -10,4 +10,5 @@ $(document).ready(function() {
   // $("body").append("<div class='container'>垂直居中模块</div>");
 });
 
-alert(require("./js/people.js"));
+// alert(require("./js/people.js"));
+console.log(require("./js/people.js"));
